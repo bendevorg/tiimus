6fadb522b2e608f4a98847e7c603bcc1e409ae6a
GuiMac <guibasconti@gmail.com>
2018-07-25 22:04:33 -0300

    Sign up form

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -17,6 +17,9 @@
         <v-card-text>
           <v-container grid-list-md>
             <v-layout wrap>
+              <v-flex xs12 v-if="signUp">
+                <v-text-field label="Name" required></v-text-field>
+              </v-flex>
               <v-flex xs12>
                 <v-text-field label="Email" required></v-text-field>
               </v-flex>
@@ -27,15 +30,31 @@
           </v-container>
         </v-card-text>
         <v-card-actions>
+          <v-btn color="blue darken-1" flat @click.native="signUp = true" v-if="!signUp">Sign up</v-btn>
+          <v-btn color="blue darken-1" flat @click.native="signUp = false" v-if="signUp">Sign in</v-btn>   
           <v-spacer></v-spacer>
           <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
-          <v-btn color="blue darken-1" flat @click.native="dialog = false">Save</v-btn>
+          <v-btn color="blue darken-1" flat @click.native="dialog = false" v-if="signUp">Sign up</v-btn>
+          <v-btn color="blue darken-1" flat @click.native="dialog = false" v-if="!signUp">Sign in</v-btn>
         </v-card-actions>
       </v-card>
       
     </v-dialog>
 
 </template>
+
+<script>
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      signUp: false
+    }
+  }
+}
+
+</script>
 
 <style lang="stylus" scoped>
   >>> .v-dialog__activator {
