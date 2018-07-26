bc9ccf4a6635e44ecf9446495118819d43c4e42d
leonardo a <leonardo.akato@gmail.com>
2018-07-25 21:45:55 -0300

    create meme page

--- layouts/default.vue
+++ layouts/default.vue
@@ -88,7 +88,8 @@
         fixed: false,
         items: [
           { icon: 'apps', title: 'Welcome', to: '/' },
-          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' },
+          { icon: 'live_tv', title: 'Meme', to: '/meme' }
         ],
         miniVariant: false,
         right: true,
---
+++ pages/meme/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-layout>
+    <v-flex text-xs-center>
+      <img src="/yo.jpg" alt="Vuetify.js" class="mb-5">
+      <blockquote class="blockquote">
+        &#8220;Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;Obama</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
