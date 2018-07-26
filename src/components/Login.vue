8e7d10a5d8dd256952b6bdb1a7ae2dbd2192e146
GuiMac <guibasconti@gmail.com>
2018-07-25 22:22:02 -0300

    Form sign in and sign up validation

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -16,17 +16,30 @@
         </v-card-title>
         <v-card-text>
           <v-container grid-list-md>
-            <v-layout wrap>
-              <v-flex xs12 v-if="signUp">
-                <v-text-field label="Name" required></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field label="Email" required></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field label="Password" type="password" required></v-text-field>
-              </v-flex>
-            </v-layout>
+            <v-form ref="form" v-model="valid" lazy-validation>
+              <v-layout wrap>
+                <v-flex xs12 v-if="signUp">
+                  <v-text-field 
+                  label="Name" 
+                  :rules="nameRules"
+                  required></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                  <v-text-field 
+                  label="Email"
+                  :rules="emailRules"
+                  type="email"
+                  required></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                  <v-text-field 
+                  label="Password"
+                  :rules="passwordRules" 
+                  type="password" 
+                  required></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-form>
           </v-container>
         </v-card-text>
         <v-card-actions>
@@ -35,7 +48,7 @@
           <v-spacer></v-spacer>
           <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
           <v-btn color="blue darken-1" flat @click.native="dialog = false" v-if="signUp">Sign up</v-btn>
-          <v-btn color="blue darken-1" flat @click.native="dialog = false" v-if="!signUp">Sign in</v-btn>
+          <v-btn color="blue darken-1" flat @click.native="submit" v-if="!signUp">Sign in</v-btn>
         </v-card-actions>
       </v-card>
       
@@ -49,7 +62,30 @@ export default {
   name: 'Login',
   data() {
     return {
-      signUp: false
+      signUp: false,
+      valid: true,
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid'
+      ],
+      name: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 10) || 'Name must be less than 10 characters'
+      ],
+      password: '',
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => (v && v.length >= 5) || 'Password must have at least 5 characters'
+      ]
+    }
+  },
+  methods: {
+    submit () {
+      if (this.$refs.form.validate()) {
+        // Native form submission is not yet supported
+      }
     }
   }
 }
