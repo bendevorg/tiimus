2f6336cfcf0049b439266e1ab9b744e2ad832f87
leonardo a <leonardo.akato@gmail.com>
2018-07-25 23:57:48 -0300

    remove nav menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,32 +1,9 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      clipped=true
-      v-model="drawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          router
-          :to="item.to"
-          :key="i"
-          v-for="(item, i) in items"
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar fixed app :clipped-left=true>
-      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+    <v-toolbar fixed app>
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
+
     </v-toolbar>
     <v-content>
       <v-container>
@@ -43,7 +20,6 @@
   export default {
     data() {
       return {
-        drawer: true,
         items: [
           { icon: 'apps', title: 'Welcome', to: '/' },
           { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' },
