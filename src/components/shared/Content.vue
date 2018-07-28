0ca8011c80137a8a7674cf6210cb31950901f512
GuiMac <guibasconti@gmail.com>
2018-07-28 07:10:24 -0300

    Add a blank footer

--- src/components/shared/Content.vue
+++ src/components/shared/Content.vue
@@ -4,16 +4,19 @@
     <v-container fluid>
       <router-view/>
     </v-container>
+    <app-footer/>
   </v-content>
 </template>
 
 <script>
 import Navmenu from './Navmenu';
+import Footer from './Footer';
 
 export default {
   name: 'Content',
   components: {
     appNavmenu: Navmenu,
+    appFooter: Footer
   }
 }
 </script>
