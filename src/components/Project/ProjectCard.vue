a794f54ebbce8275d8c8374a15e0e0d2493116f2
GuiMac <guibasconti@gmail.com>
2018-07-31 13:31:20 -0300

    Remove join button

--- src/components/Project/ProjectCard.vue
+++ src/components/Project/ProjectCard.vue
@@ -18,7 +18,6 @@
     </v-card-title>
 
     <v-card-actions>
-      <v-btn flat>Join</v-btn>
       <v-spacer></v-spacer>
       <v-btn icon @click="show = !show">
         <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
