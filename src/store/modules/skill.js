2651bb4f57846356266d5e2dc386f543fd391780
GuiMac <guibasconti@gmail.com>
2018-09-05 18:20:15 -0300

    Adapt mock responses to be equal to API

---
+++ src/store/modules/skill.js
@@ -0,0 +1,43 @@
+import API from '../../utils/API';
+
+const state = {
+  skills: []
+};
+
+const getters = {
+  retrieveSkillColor: state => skill => {
+    const skillIndex = state.skills.findIndex(currentSkill => currentSkill.name === skill);
+    return skillIndex !== -1 ? state.skills[skillIndex].color : 'black'
+  },
+  retrieveSkillTextColor: state => skill => {
+    const skillIndex = state.skills.findIndex(currentSkill => currentSkill.name === skill);
+    return skillIndex !== -1 ? state.skills[skillIndex].text : 'white'
+  }
+};
+
+const actions = {
+  listSkills({ commit }) {
+    API
+      .get('/skills')
+      .then(response => {
+        commit('setSkills', response.data.msg);
+      })
+      .catch(error => {
+        //  TODO: Handle the error
+      });
+  }
+};
+
+const mutations = {
+  setSkills(state, skills) {
+    state.skills = skills;
+  }
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+};
--- src/utils/mock.js
+++ src/utils/mock.js
@@ -25,60 +25,79 @@ export default axios => {
   mock
     .onGet('/projects')
     .reply(200, {
-      msg: {
-        projects: [
-          { 
-            title: 'Brawll', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', 
-            skills: ['Developer', 'Artist', 'Musician'],
-            description: 'Brawll is an action 3D game where your goal is to send the other players to outer space.',
-            flex: 3
-          },
-          { 
-            title: 'Jacto Rally', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', 
-            skills: ['Writer'],
-            description: 'Brawll is an action 3D game where your goal is to send the other players to outer space.',
-            flex: 3 
-          },
-          { 
-            title: 'Into the light', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
-            skills: ['Developer', 'Audio Mixer'],
-            description: 'Brawll is an action 3D game where your goal is to send the other players to outer space.',
-            flex: 3 },
-          { 
-            title: 'Disk Overdrive', src: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg', 
-            skills: ['Artist'],
-            description: 'Brawll is an action 3D game where your goal is to send the other players to outer space.',
-            flex: 3 
-          }
-        ]
-      }
+      msg: [
+        { 
+          title: 'Brawll', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', 
+          skills: ['Developer', 'Artist', 'Musician'],
+          tags: [],
+          description: 'Brawll is an action 3D game where your goal is to send the other players to outer space.',
+          flex: 3
+        },
+        { 
+          title: 'Jacto Rally', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', 
+          skills: ['Writer'],
+          tags: [],
+          description: 'Brawll is an action 3D game where your goal is to send the other players to outer space.',
+          flex: 3 
+        },
+        { 
+          title: 'Into the light', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
+          skills: ['Developer', 'Audio Mixer'],
+          tags: [],
+          description: 'Brawll is an action 3D game where your goal is to send the other players to outer space.',
+          flex: 3 
+        },
+        { 
+          title: 'Disk Overdrive', src: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg', 
+          skills: ['Artist'],
+          tags: [],
+          description: 'Brawll is an action 3D game where your goal is to send the other players to outer space.',
+          flex: 3 
+        }
+      ]
     });
 
     mock
       .onGet('/tags')
       .reply(200, {
-        msg: {
-          tags: [
-            'Action',
-            'Strategy',
-            '2D',
-            '3D'
-          ]
-        }
+        msg: [
+          'Action',
+          'Strategy',
+          '2D',
+          '3D'
+        ]
       });
 
     mock
       .onGet('/skills')
       .reply(200, {
-        msg: {
-          skills: [
-            'Artist',
-            'Developer',
-            'Musician',
-            'Writer',
-            'Audio Mixer'
-          ]
-        }
+        msg: [
+          {
+            name: 'Developer',
+            color: 'grey lighten-1',
+            text: 'black'
+          },
+          {
+            name:'Artist',
+            color: 'grey darken-1',
+            text: 'white'
+          },
+          {
+            name: 'Musician',
+            color: 'grey darken-2',
+            text: 'white'
+          },
+          {
+            name: 'Writer',
+            color: 'grey darken-3',
+            text: 'white'
+          },
+          {
+            name: 'Audio Mixer',
+            color: 'grey darken-4',
+            text: 'white'
+          }
+        ]
       });
 
 };
