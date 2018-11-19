ac61ba6f0b4c45a5d36d9f084e532fb5d0c3e20f
Guilherme Conti <guibasconti@gmail.com>
2018-11-19 16:54:59 -0200

    Disable host check at dev

--- tools/webpack.dev.conf.js
+++ tools/webpack.dev.conf.js
@@ -35,6 +35,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     hot: true,
     contentBase: false, // since we use CopyWebpackPlugin.
     compress: true,
+    disableHostCheck: true,
     host: HOST || config.dev.host,
     port: PORT || config.dev.port,
     open: config.dev.autoOpenBrowser,
