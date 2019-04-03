31db820ef27b8bfe9f13ea6a5696f10ad30a2bc0
Michel Makarios <michel.makarios@gmail.com>
2019-04-03 20:14:22 -0300

    Fix name from Buildev to Tiimus

--- src/components/shared/Footer.vue
+++ src/components/shared/Footer.vue
@@ -2,8 +2,8 @@
   <v-footer app height="auto" class="grey darken-3">
     <v-layout row wrap justify-center>
       <v-flex xs12 py-3 text-xs-center white--text>
-        &copy;2018 —
-        <strong>Buildev</strong>
+        &copy;2019 —
+        <strong>Tiimus</strong>
       </v-flex>
     </v-layout>
   </v-footer>
