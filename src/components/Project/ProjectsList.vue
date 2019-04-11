1484a3c15de6eb93816d523cc1660b7405078106
GuiMac <guibasconti@gmail.com>
2019-04-11 11:48:16 -0300

    Limit cards being show at project lists. Closes #52

--- src/components/Project/ProjectsList.vue
+++ src/components/Project/ProjectsList.vue
@@ -5,8 +5,16 @@
         <div class="headline">{{ title }}</div>
         <div v-if="projects && projects.length > 0">
           <v-layout row wrap>
-            <v-flex v-for="project in projects" :key="project.id" xs12 sm6 md3>
-              <app-project-card :project="project" :reduced="reduced" />
+            <v-flex v-for="index in cardsToShow" :key="projects[index - 1].id" xs12 sm6 md3>
+              <app-project-card :project="projects[index - 1]" :reduced="reduced" />
+            </v-flex>
+            <v-flex xs12 class="text-xs-center">
+              <v-btn 
+                v-if="cardsToShow < projects.length"
+                @click="showMoreCards()"
+              >
+                Show more
+              </v-btn>
             </v-flex>
           </v-layout>
         </div>
@@ -42,10 +50,26 @@ export default {
       required: false,
       default: 'No projects'
     },
+    amountToShow: {
+      type: Number,
+      required: false,
+      default: 8
+    },
     reduced: {
       type: Boolean,
       required: false,
       default: false
+    }
+  },
+  data() {
+    return {
+      cardsToShow: this.amountToShow
+    };
+  },
+  methods: {
+    showMoreCards() {
+      this.cardsToShow = this.cardsToShow + this.amountToShow < this.projects.length ?
+        this.cardsToShow + this.amountToShow : this.projects.length;
     }
   }
 };
