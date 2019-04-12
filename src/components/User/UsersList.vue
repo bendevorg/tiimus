abca981a5a42417e8d34ea64dce55cb384837661
Guilherme Conti <guibasconti@gmail.com>
2019-04-12 13:23:40 -0300

    Add show more at users list. Closes #53

--- src/components/User/UsersList.vue
+++ src/components/User/UsersList.vue
@@ -5,8 +5,16 @@
         <div class="headline">{{ title }}</div>
         <div v-if="users && users.length > 0">
           <v-layout row wrap>
-            <v-flex v-for="user in users" :key="user.id" xs12 sm6 md3>
-              <app-user-card :user="user" />
+            <v-flex v-for="index in cardsToShow" :key="users[index - 1].id" xs12 sm6 md3>
+              <app-user-card :user="users[index - 1]"/>
+            </v-flex>
+            <v-flex xs12 class="text-xs-center">
+              <v-btn 
+                v-if="cardsToShow < users.length"
+                @click="showMoreCards()"
+              >
+                Show more
+              </v-btn>
             </v-flex>
           </v-layout>
         </div>
@@ -41,6 +49,30 @@ export default {
       type: String,
       required: true,
       default: 'No users'
+    },
+    amountToShow: {
+      type: Number,
+      required: false,
+      default: 8
+    },
+  },
+  data() {
+    return {
+      moreCards: 0
+    };
+  },
+  computed: {
+    cardsToShow: function() {
+      return this.users ? 
+        (this.users.length > this.amountToShow + this.moreCards ?
+        this.amountToShow + this.moreCards : this.users.length)
+        : 0;
+    }
+  },
+  methods: {
+    showMoreCards() {
+      this.moreCards = this.moreCards + this.cardsToShow + this.amountToShow < this.users.length ?
+        this.moreCards + this.amountToShow : this.users.length;
     }
   }
 };
