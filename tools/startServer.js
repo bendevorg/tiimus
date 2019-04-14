6eeacf6ff718e05c59de12d47113f1447da54788
Guilherme Conti <guibasconti@gmail.com>
2019-04-14 02:13:40 -0300

    Fix production start

--- tools/startServer.js
+++ tools/startServer.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app =
-  process.env.NODE_ENV == 'production' ? require('./serverProduction') : null;
+  process.env.NODE_ENV == '"production"' ? require('./serverProduction') : null;
 
 const PORT = process.env.PORT;
 
