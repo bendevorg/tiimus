470772ef35633135dccc2c4de714e8231e9f321b
GuiMac <guibasconti@gmail.com>
2019-04-16 21:10:29 -0300

    Handle routing poorly for index

--- tools/serverProduction.js
+++ tools/serverProduction.js
@@ -8,5 +8,6 @@ const express = require('express');
 const app = express();
 
 app.use('/', express.static('dist'));
+app.use(['/users/*', '/user/*', '/projects/*', '/project/*'], express.static('dist/index.html'));
 
 module.exports = app;
